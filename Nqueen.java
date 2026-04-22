940ecadec79b91af9991ae795542d72078cb184f
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-04-22 22:57:44 +0530

    Add files via upload

---
+++ Nqueen.java
@@ -0,0 +1,75 @@
+public class Nqueen {
+
+    public static boolean isSafe(char board[][], int row, int col) {
+
+        // vertical up
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        // left diagonal
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        // right diagonal
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void solveNQueen(char board[][], int row){
+
+        if(row == board.length){
+        // printBoard(board); 
+           count++;
+            return;
+        }
+
+        for(int j=0; j<board.length; j++){
+
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+
+                solveNQueen(board, row+1);
+
+                board[row][j] = 'x'; // backtracking
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]){
+        System.out.println("-----Board-----");
+
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static int count=0;
+
+    public static void main(String[] args){
+
+        int n =10;
+        char board[][] = new char[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = 'x';
+            }
+        }
+
+        solveNQueen(board, 0);
+        System.out.println("total no of place queen"  +count);
+    }
+}
